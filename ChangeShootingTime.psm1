181b69a4296d3b233e2d4b548682c2317f6f5695
Syuichi Murashima <mura@vwnet.jp>
2021-09-25 15:12:37 +0900

    1st commit

---
+++ ChangeShootingTime.psm1
@@ -0,0 +1,70 @@
+﻿#######################################################
+# Jpeg の撮影時間を変更する
+#######################################################
+function ChangeShootingTime( $JpegFile, $Offset ){
+	# 拡張子チェック
+	$FileName = Split-Path $JpegFile -Leaf
+	if( (($FileName -split "\.")[1] -ne "jpg" ) -and (($FileName -split "\.")[1] -ne "jpeg" )){
+		return
+	}
+
+	# フルパスにする
+	$JpegFileFullName = Convert-Path $JpegFile -ErrorAction SilentlyContinue
+	if( $JpegFileFullName -eq $null ){
+		echo "$JpegFile not convert full path."
+		return
+	}
+
+	# 存在確認
+	if( -not ( Test-Path $JpegFileFullName )){
+		echo "$JpegFile not found"
+		return
+	}
+
+	# テンポラリファイル名
+	$TempFile = $JpegFileFullName + ".tmp"
+
+	# アセンブリロード
+	Add-Type -AssemblyName System.Drawing
+
+	# Jpeg 読み込み
+	$bmp = New-Object System.Drawing.Bitmap($JpegFileFullName)
+
+	# 撮影日時
+	$DateTimeOriginalIndex = 0x9003
+
+	Try{
+		$DateTimeOriginal = $bmp.GetPropertyItem($DateTimeOriginalIndex)
+	}
+	Catch{
+		$bmp.Dispose()
+		return
+	}
+
+	[array]$DateTimeOriginalBytes = $DateTimeOriginal.Value
+
+	$DateString = [System.Text.Encoding]::ASCII.GetString($DateTimeOriginalBytes)
+
+	[array]$YYYYMMDDandHHMMSS = $DateString.Split(" ")
+	[datetime]$OriginalDateTime = $YYYYMMDDandHHMMSS[0].Replace(":","/") + " " + $YYYYMMDDandHHMMSS[1]
+	[datetime]$NewDateTime = $OriginalDateTime.AddHours(-9)
+	$NewDateTimeString = $NewDateTime.ToString("yyyy:MM:dd HH:mm:ss")
+	[array] $NewDateBytes = [System.Text.Encoding]::ASCII.GetBytes($NewDateTimeString)
+
+	$NewDateBytes += 0x00
+
+	$DateTimeOriginal.Value = $NewDateBytes
+
+	$bmp.SetPropertyItem($DateTimeOriginal)
+
+	# ファイル出力
+	$bmp.Save($TempFile, [System.Drawing.Imaging.ImageFormat]::Jpeg )
+	$bmp.Dispose()
+
+	# オリジナルとテンポラリファイルを入れ替える
+	$Org_Path = Split-Path -Path $JpegFileFullName
+	$Org_FileName = Split-Path -Leaf $JpegFileFullName
+	$New_FileName = $Org_FileName.Split(".")[0] + "-ORG." + $Org_FileName.Split(".")[1]
+	ren $JpegFileFullName $New_FileName
+	ren $TempFile $JpegFileFullName
+}
---
+++ ReadMe.txt
@@ -0,0 +1,35 @@
+﻿Jpeg 画像方向を撮影時間を変更する
+
+■ これは何?
+Jpeg 画像の撮影時間をオフセットで変更します
+写真の撮影日時が UTC になっているのを JST に変更するとか、撮影時間を変更します
+
+■ 使い方
+PowerShell プロンプトで以下コマンド入力すると、Jpeg の撮影時間がオフセット時間分変更されます
+オリジナルファイルは -ORG.jpg として残ります
+
+ChangeShootingTime ファイルPath オフセット時間
+
+(例)
+ChangeShootingTime "C:\temp\20210919_224409029_iOS.jpg" -9
+
+■ セットの仕方
+PowerShell プロンプトで install.ps1 を実行してください
+
+■ Uninstall 方法
+uninstall.ps1 を実行して下さい
+
+■ 動作確認環境
+On Windows
+	5.1
+	7.1.4
+
+■ Web サイト
+Jpeg ファイルの GPS Exif を PowerShell で削除する
+https://www.vwnet.jp/Windows/PowerShell/2016051501/RemoveGPSExif.htm
+
+■ リポジトリ
+https://github.com/MuraAtVwnet/ChangeShootingTime
+git@github.com:MuraAtVwnet/ChangeShootingTime.git
+
+
